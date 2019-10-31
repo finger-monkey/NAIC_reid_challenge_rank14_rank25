baa5cc50b8a3469efb92a6da0954ba0ac191f50a
anxiang <anxiangsir@outlook.com>
2019-10-31 20:11:07 +0800

    init commit

--- test.sh
+++ test.sh
@@ -1,1 +1,11 @@
-python tools/test.py --config_file='configs/softmax_triplet_with_center.yml' MODEL.DEVICE_ID "('1')" DATASETS.NAMES "('dukemtmc')" DATASETS.ROOT_DIR "('../train_split/split2')" MODEL.IF_WITH_CENTER "('yes')" MODEL.METRIC_LOSS_TYPE "('triplet_center')" TEST.NECK_FEAT "('after')" TEST.FEAT_NORM "('yes')" TEST.WEIGHT "('exps/Experiment1/se_resnet101_model_120.pth')" OUTPUT_DIR "('./exps/test')"
+python tools/test.py \
+  --config_file='configs/softmax_triplet_with_center_anxiang.yml' \
+  MODEL.DEVICE_ID "('2')" \
+  DATASETS.NAMES "('dukemtmc')" \
+  DATASETS.ROOT_DIR "('/home/xiangan/code_and_data/train_split/split2')" \
+  MODEL.IF_WITH_CENTER "('yes')" \
+  MODEL.METRIC_LOSS_TYPE "('triplet_center')" \
+  TEST.NECK_FEAT "('after')" \
+  TEST.FEAT_NORM "('yes')" \
+  TEST.WEIGHT "('/mnt/anxiang/models/reid_exp/experiment2/se_resnet101_center_param_120.pth')" \
+  OUTPUT_DIR "('./exps/test')"
