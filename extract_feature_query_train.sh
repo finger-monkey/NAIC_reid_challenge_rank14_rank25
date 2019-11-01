8589b0aaeedf1eeb7b2f74bf3e5e7bd6f11ca97e
anxiang <anxiangsir@outlook.com>
2019-11-01 13:32:41 +0800

    init commit

--- extract_feature_query_train.sh
+++ extract_feature_query_train.sh
@@ -4,5 +4,5 @@ MODEL.DEVICE_ID "('1')" \
 DATASETS.NAMES "('dukemtmc')" \
 DATASETS.ROOT_DIR "('/home/xiangan/code_and_data/train_split/split2')" \
 MODEL.PRETRAIN_CHOICE "('self')" \
-MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/experiment2/se_resnet101_center_param_120.pth')" \
+MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/experiment2/se_resnet101_model_120.pth')" \
 OUTPUT_DIR "('./exps/test')"
