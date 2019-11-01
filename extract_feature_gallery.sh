99921bea52034059a26b357f3e29031c4e11df25
anxiang <anxiangsir@outlook.com>
2019-11-01 12:35:02 +0800

    init commit

--- extract_feature_gallery.sh
+++ extract_feature_gallery.sh
@@ -1,8 +1,8 @@
 python tools/extract_feature_gallery.py \
-  --config_file='configs/softmax_triplet_with_center.yml' \
-  MODEL.DEVICE_ID "('1')" \
-  DATASETS.NAMES "('dukemtmc')" \
-  DATASETS.ROOT_DIR "('../train_split/split2')" \
-  MODEL.PRETRAIN_CHOICE "('self')" \
-  MODEL.PRETRAIN_PATH "('exps/Experiment1/se_resnet101_model_120.pth')" \
-  OUTPUT_DIR "('./exps/test')"
+--config_file='configs/softmax_triplet_with_center_anxiang.yml' \
+MODEL.DEVICE_ID "('1')" \
+DATASETS.NAMES "('dukemtmc')" \
+DATASETS.ROOT_DIR "('/home/xiangan/code_and_data/train_split/split2')" \
+MODEL.PRETRAIN_CHOICE "('self')" \
+MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/experiment2/se_resnet101_center_param_120.pth')" \
+OUTPUT_DIR "('./exps/test')"
