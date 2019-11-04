da4aefaf5193a5f7cd1dd57bca9143f753e2c2c4
anxiang <anxiangsir@outlook.com>
2019-11-04 18:16:53 +0800

    init commit

--- extrach_feature_test.sh
+++ extrach_feature_test.sh
@@ -1,10 +1,9 @@
+python extraect_feature_test.py \
+  --config_file='configs/softmax_triplet_with_center_anxiang.yml' \
+  MODEL.DEVICE_ID "('1')" \
+  DATASETS.NAMES "('dukemtmc')" \
+  DATASETS.ROOT_DIR "('/home/xiangan/data_reid/testA')" \
+  MODEL.PRETRAIN_CHOICE "('self')" \
+  MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/experiment2/se_resnet101_model_120.pth')" \
+  OUTPUT_DIR "('./exps/test')"
 
-
-python tools/extract_feature_query_train.py \
---config_file='configs/softmax_triplet_with_center_anxiang.yml' \
-MODEL.DEVICE_ID "('1')" \
-DATASETS.NAMES "('dukemtmc')" \
-DATASETS.ROOT_DIR "('/home/xiangan/code_and_data/train_split/split2')" \
-MODEL.PRETRAIN_CHOICE "('self')" \
-MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/experiment2/se_resnet101_model_120.pth')" \
-OUTPUT_DIR "('./exps/test')"
--- extract_feature_gallery.sh
+++
@@ -1,8 +0,0 @@
-python tools/extract_feature_gallery.py \
---config_file='configs/softmax_triplet_with_center_anxiang.yml' \
-MODEL.DEVICE_ID "('1')" \
-DATASETS.NAMES "('dukemtmc')" \
-DATASETS.ROOT_DIR "('/home/xiangan/code_and_data/train_split/split2')" \
-MODEL.PRETRAIN_CHOICE "('self')" \
-MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/experiment2/se_resnet101_model_120.pth')" \
-OUTPUT_DIR "('./exps/test')"
