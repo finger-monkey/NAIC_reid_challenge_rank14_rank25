f26b80e30ef727fe217a2e1ac4c5c345929e45ec
anxiang <anxiangsir@outlook.com>
2019-11-05 23:16:16 +0800

    fix

--- extrach_feature_test.sh
+++ extrach_feature_test.sh
@@ -1,9 +1,9 @@
 python extraect_feature_test.py \
-  --config_file='configs/senet50_without_centerloss.yml' \
+  --config_file='configs/senet50_MGN_2.yml' \
   MODEL.DEVICE_ID "('1')" \
   DATASETS.NAMES "('dukemtmc')" \
   DATASETS.ROOT_DIR "('/home/xiangan/data_reid/testA')" \
   MODEL.PRETRAIN_CHOICE "('self')" \
-  MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/all_11_4_withoutcenterloss/se_resnet50_model_120.pth')" \
-  OUTPUT_DIR "('./exps/all_11_4_withoutcenterloss')"
+  MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/mgn_noaug/se_resnet50_model_120.pth')" \
+  OUTPUT_DIR "('./exps/all_11_5_mgn')"
 
