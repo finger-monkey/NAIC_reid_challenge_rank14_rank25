c2c1110e68f6eba9a6e15437f643cc0884e36b1e
anxiang <anxiangsir@outlook.com>
2019-11-06 22:53:04 +0800

    add labelsmooth

--- extrach.sh
+++
@@ -1,9 +0,0 @@
-python extract.py \
-  --config_file='configs/mgn_baseline.yml' \
-  MODEL.DEVICE_ID "('1')" \
-  DATASETS.NAMES "('dukemtmc')" \
-  DATASETS.ROOT_DIR "('/home/xiangan/data_reid/testA')" \
-  MODEL.PRETRAIN_CHOICE "('self')" \
-  MODEL.PRETRAIN_PATH "('/mnt/anxiang/models/reid_exp/mgn_noaug/se_resnet50_model_120.pth')" \
-  OUTPUT_DIR "('./exps/all_11_5_mgn')"
-
