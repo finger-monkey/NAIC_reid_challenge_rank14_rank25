1ca7951325cc862a223a334d5eb9ff28f0bc4789
anxiang <anxiangsir@outlook.com>
2019-11-06 14:16:47 +0800

    init commit

---
+++ train.sh
@@ -0,0 +1,14 @@
+
+CONFIG_FILE = "configs/${s1}.yml"
+
+echo CONFIG_FILE
+
+
+
+
+#python tools/train.py \
+#  --config_file='configs/senet50_MGN_2.yml' \
+#  MODEL.DEVICE_ID "('0,1')" \
+#  DATASETS.NAMES "('dukemtmc')" \
+#  DATASETS.ROOT_DIR "('/home/xiangan/code_and_data/train_split/all')" \
+#  OUTPUT_DIR "('/mnt/anxiang/models/reid_exp/mgn_noaug')"
