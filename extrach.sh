c22b620d07c7fbc3ed075a5b9481f461e7934a43
anxiang <anxiangsir@outlook.com>
2019-11-06 22:52:40 +0800

    add labelsmooth

--- extrach.sh
+++ extrach.sh
@@ -1,5 +1,5 @@
 python extract.py \
-  --config_file='configs/mgn.yml' \
+  --config_file='configs/mgn_baseline.yml' \
   MODEL.DEVICE_ID "('1')" \
   DATASETS.NAMES "('dukemtmc')" \
   DATASETS.ROOT_DIR "('/home/xiangan/data_reid/testA')" \
