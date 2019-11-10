8daef0d8917305e43579c382d7f4d03a59919a2e
anxiang <anxiangsir@outlook.com>
2019-11-10 23:57:07 +0800

    add labelsmooth

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 # config_file
-config_file="configs/${1}.yml"
+config_file=configs/${1}.yml
 # output_dir
-output_dir="('/mnt/anxiang/models/reid_exp/${1}')"
+output_dir=/mnt/anxiang/models/reid_exp/${1}
 # train_root
-train_root="('/home/xiangan/code_and_data/train_split/all')"
+train_root=/home/xiangan/code_and_data/train_split/all
 # test_root
-test_root="('/home/xiangan/data_reid/testA')"
+test_root=/home/xiangan/data_reid/testA
 # model_pretrain_path
-model_pretrain_path="(${output_dir}/se_resnet50_model_80.pth')"
+model_pretrain_path=${output_dir}/se_resnet50_model_80.pth
 
 echo "config files is ${config_file}"
 echo "save path is ${output_dir}"
@@ -17,17 +17,15 @@ echo "save path is ${output_dir}"
 python tools/train.py \
     --config_file=${config_file} \
     MODEL.DEVICE_ID "('0,1')" \
-    DATASETS.NAMES "('dukemtmc')" \
-    DATASETS.ROOT_DIR ${train_root} \
-    OUTPUT_DIR ${output_dir}
+    DATASETS.ROOT_DIR "('${train_root}')" \
+    OUTPUT_DIR "('${output_dir}')"
 
 python extract.py \
     --config_file=${config_file} \
     MODEL.DEVICE_ID "('0')" \
-    DATASETS.NAMES "('dukemtmc')" \
     DATASETS.ROOT_DIR ${test_root} \
     MODEL.PRETRAIN_CHOICE "('self')" \
-    MODEL.PRETRAIN_PATH ${model_pretrain_path} \
+    MODEL.PRETRAIN_PATH "('${model_pretrain_path}')" \
     OUTPUT_DIR "('./features/${1}')"
 
 python get_submission.py --name "${1}"
