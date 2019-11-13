130c55a72f1be7aac9e56898faa7644aca05c452
anxiang <anxiangsir@outlook.com>
2019-11-13 12:14:10 +0800

    add reduction

--- val.sh
+++ val.sh
@@ -12,6 +12,6 @@ echo "save path is ${output_dir}"
 
 python tools/train.py \
     --config_file=${config_file} \
-    MODEL.DEVICE_ID "('0,1')" \
+    MODEL.DEVICE_ID "(${2})" \
     DATASETS.ROOT_DIR "('${train_root}')" \
     OUTPUT_DIR "('${output_dir}')"
