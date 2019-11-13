3dd3e38f26375460a16aa26a14dedab0c9437cd8
anxiang <anxiangsir@outlook.com>
2019-11-13 17:12:06 +0800

    add reduction

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -5,15 +5,29 @@
 """
 
 from .baseline import Baseline
+from .bnneck_mgn import BNNeck_MGN
 
 
 def build_model(cfg, num_classes):
-    model = Baseline(
-        num_classes=num_classes,
-        last_stride=cfg.MODEL.LAST_STRIDE,
-        model_path=cfg.MODEL.PRETRAIN_PATH,
-        model_name=cfg.MODEL.NAME,
-        pretrain_choice=cfg.MODEL.PRETRAIN_CHOICE,
-        reduction=cfg.MODEL.REDUCTION
-    )
-    return model
+    if cfg.MODEL.BNNECK == 'yes':
+        model = BNNeck_MGN(
+            num_classes=num_classes,
+            last_stride=cfg.MODEL.LAST_STRIDE,
+            model_path=cfg.MODEL.PRETRAIN_PATH,
+            model_name=cfg.MODEL.NAME,
+            pretrain_choice=cfg.MODEL.PRETRAIN_CHOICE,
+            reduction=cfg.MODEL.REDUCTION
+        )
+        return model
+    elif cfg.MODEL.BNNECK == 'no':
+        model = Baseline(
+            num_classes=num_classes,
+            last_stride=cfg.MODEL.LAST_STRIDE,
+            model_path=cfg.MODEL.PRETRAIN_PATH,
+            model_name=cfg.MODEL.NAME,
+            pretrain_choice=cfg.MODEL.PRETRAIN_CHOICE,
+            reduction=cfg.MODEL.REDUCTION
+        )
+        return model
+    else:
+        raise ValueError
