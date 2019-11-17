8c3436ccc98aa1633ee59dee40748d3e978728cb
anxiang <anxiangsir@outlook.com>
2019-11-17 17:14:55 +0800

    add labelsmooth

--- data/transforms/build.py
+++ data/transforms/build.py
@@ -7,6 +7,7 @@
 import torchvision.transforms as T
 
 from .transforms import RandomErasing
+from .transforms import RandomSizedRectCrop
 
 
 def build_transforms(cfg, is_train=True):
@@ -16,10 +17,11 @@ def build_transforms(cfg, is_train=True):
             T.Resize(cfg.INPUT.SIZE_TRAIN),
             T.RandomHorizontalFlip(p=cfg.INPUT.PROB),
             T.Pad(cfg.INPUT.PADDING),
-            T.RandomCrop(cfg.INPUT.SIZE_TRAIN),
+            # T.RandomCrop(cfg.INPUT.SIZE_TRAIN),
+            RandomSizedRectCrop(height=384, width=128),
             T.ToTensor(),
             normalize_transform,
-            RandomErasing(probability=cfg.INPUT.RE_PROB, mean=cfg.INPUT.PIXEL_MEAN)
+            RandomErasing(probability=cfg.INPUT.RE_PROB, mean=(0.0972, 0.1831, 0.2127))
         ])
     else:
         transform = T.Compose([
