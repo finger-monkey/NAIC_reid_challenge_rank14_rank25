504e4f8e1d486fd8780359fa0060af285268da26
anxiang <anxiangsir@outlook.com>
2019-11-19 19:11:56 +0800

    add 030

--- data/transforms/build.py
+++ data/transforms/build.py
@@ -7,17 +7,29 @@
 import torchvision.transforms as T
 
 from .transforms import RandomErasing
-from .transforms import RandomSizedRectCrop
+from .transforms import Random2DTranslation
 
 
 def build_transforms(cfg, is_train=True):
+    #
     normalize_transform = T.Normalize(mean=cfg.INPUT.PIXEL_MEAN, std=cfg.INPUT.PIXEL_STD)
+
+    #
+    if cfg.INPUT.CROP == 'norm':
+        CROP = T.RandomCrop(cfg.INPUT.SIZE_TRAIN)
+    elif cfg.INPUT.CROP == 'random':
+        CROP = Random2DTranslation(
+            height=cfg.INPUT.SIZE_TRAIN[0],
+            width=cfg.INPUT.SIZE_TRAIN[1])
+    else:
+        raise ValueError
+
     if is_train:
         transform = T.Compose([
             T.Resize(cfg.INPUT.SIZE_TRAIN),
             T.RandomHorizontalFlip(p=cfg.INPUT.PROB),
             T.Pad(cfg.INPUT.PADDING),
-            T.RandomCrop(cfg.INPUT.SIZE_TRAIN),
+            CROP(cfg.INPUT.SIZE_TRAIN),
             # RandomSizedRectCrop(height=384, width=128),
             T.ToTensor(),
             normalize_transform,
