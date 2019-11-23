de4eaec915a8587f6fa165613c5a9fc0251ae84f
anxiang <anxiangsir@outlook.com>
2019-11-23 23:15:59 +0800

    reran

--- ensemble_submit.py
+++ ensemble_submit.py
@@ -39,13 +39,13 @@ def get(FEATURE, query_imgnames_1, gallery_imgnames_1):
 
 
 def main():
-    FEATURE_1 = "/home/xiangan/dgreid/features/026"
-    FEATURE_2 = "/home/xiangan/dgreid/features/036"
-    FEATURE_3 = "/home/xiangan/dgreid/features/044"
-    FEATURE_4 = "/home/xiangan/dgreid/features/040"
-    FEATURE_5 = "/home/xiangan/dgreid/features/046"
-    FEATURE_6 = "/home/xiangan/dgreid/features/039"
-    FEATURE_7 = "/home/xiangan/dgreid/features/feature_BFE_52"
+    FEATURE_1 = "/home/xiangan/dgreid/features/054"
+    FEATURE_2 = "/home/xiangan/dgreid/features/055"
+    FEATURE_3 = "/home/xiangan/dgreid/features/056"
+    FEATURE_4 = "/home/xiangan/dgreid/features/0102"
+    # FEATURE_5 = "/home/xiangan/dgreid/features/046"
+    # FEATURE_6 = "/home/xiangan/dgreid/features/039"
+    # FEATURE_7 = "/home/xiangan/dgreid/features/feature_BFE_52"
 
     # feature_1
     gallery_info_1 = pickle.load(open('%s/gallery_a_feature.feat' % FEATURE_1, 'rb'))
@@ -56,12 +56,12 @@ def main():
     qf_2, gf_2 = get(FEATURE_2, query_imgnames_1, gallery_imgnames_1)
     qf_3, gf_3 = get(FEATURE_3, query_imgnames_1, gallery_imgnames_1)
     qf_4, gf_4 = get(FEATURE_4, query_imgnames_1, gallery_imgnames_1)
-    qf_5, gf_5 = get(FEATURE_5, query_imgnames_1, gallery_imgnames_1)
-    qf_6, gf_6 = get(FEATURE_6, query_imgnames_1, gallery_imgnames_1)
-    qf_7, gf_7 = get(FEATURE_7, query_imgnames_1, gallery_imgnames_1)
+    # qf_5, gf_5 = get(FEATURE_5, query_imgnames_1, gallery_imgnames_1)
+    # qf_6, gf_6 = get(FEATURE_6, query_imgnames_1, gallery_imgnames_1)
+    # qf_7, gf_7 = get(FEATURE_7, query_imgnames_1, gallery_imgnames_1)
 
-    query_feats = np.concatenate((query_feats_1, qf_2, qf_3, qf_4, qf_5, qf_6, qf_7), axis=1)
-    gallery_feats = np.concatenate((gallery_feats_1, gf_2, gf_3, gf_4, gf_5, gf_6, gf_7), axis=1)
+    query_feats = np.concatenate((query_feats_1, qf_2, qf_3, qf_4), axis=1)
+    gallery_feats = np.concatenate((gallery_feats_1, gf_2, gf_3, gf_4), axis=1)
     query_feats = preprocessing.normalize(query_feats)
     gallery_feats = preprocessing.normalize(gallery_feats)
 
@@ -85,7 +85,7 @@ def main():
     submission_json = json.dumps(submission_key)
     print(type(submission_json))
 
-    with open('ensemblex7.json', 'w', encoding='utf-8') as f:
+    with open('11_23_rerank1.json', 'w', encoding='utf-8') as f:
         f.write(submission_json)
 
 
