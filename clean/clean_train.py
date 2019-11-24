7e64ce61bb34e9551fe7a734ad9d01674b633fb7
anxiang <anxiangsir@outlook.com>
2019-11-25 00:20:48 +0800

    reran

--- clean/clean_train.py
+++ clean/clean_train.py
@@ -18,4 +18,16 @@ def process_info(info):
 
 
 def main():
-    pass
+    # path
+    feature_path = "/home/xiangan/dgreid/features/trainset_features_056/trainset.feat"
+    # info
+    feat_info = pickle.load(open(feature_path, 'rb'))
+    # feats and names
+    feats, image_name_list = process_info(feat_info)
+
+    for image_name in image_name_list:
+        print(image_name)
+
+
+if __name__ == '__main__':
+    main()
