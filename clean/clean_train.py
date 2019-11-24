da6d1fb3f97fb10018de37c6381fe9d1989b7a07
anxiang <anxiangsir@outlook.com>
2019-11-25 00:38:14 +0800

    reran

--- clean/clean_train.py
+++ clean/clean_train.py
@@ -25,12 +25,43 @@ def main():
     # feats and names
     feats, image_name_list = process_info(feat_info)
 
+    #
     # for image_name in image_name_list:
     #     print(image_name)
 
+    ID_NUM_DICT = {
+        '1055': 610,
+        '0767': 1018,
+        '0968': 118,
+        '0760': 122,
+        '1374': 731,
+        '0174': 306,
+        '0383': 774,
+        '1161': 113,
+        '1477': 864,
+        '1273': 223,
+        '1350': 291,
+        '0651': 188,
+        '0514': 122,
+        '0112': 150
+    }
 
+    count_dict = {}
+    for item in image_name_list:
+        # 0057_c1_928644343.png
+        pid = item.split('_')[0]
 
+        # count
+        if pid not in count_dict:
+            count_dict[pid] = 1
+        else:
+            count_dict[pid] += 1
 
+    # check
+    for pid, count in ID_NUM_DICT.items():
+        assert count_dict[pid] == count
+
+    
 
 
 if __name__ == '__main__':
