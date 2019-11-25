dd02eb2ae3ea8b6ad4ea7ae15203eb0d3c5e3fae
anxiang <anxiangsir@outlook.com>
2019-11-25 14:04:23 +0800

    reran

--- clean/clean_repeat.py
+++ clean/clean_repeat.py
@@ -94,7 +94,7 @@ def main():
 
             repeat_num = np.sum(curr_distance_matrix > threshold)
 
-            if repeat_num > 3:
+            if repeat_num > 10:
                 #
                 clean_id_set.add(id_image)
 
