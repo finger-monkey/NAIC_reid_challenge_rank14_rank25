b6c693649c9f2052cdc7f4bfae8207ba35765694
anxiang <anxiangsir@outlook.com>
2019-11-28 15:13:00 +0800

    reran

--- semi_supervised_learning.py
+++ semi_supervised_learning.py
@@ -100,7 +100,7 @@ def main():
 
     QUERY_FEAT = np.concatenate((testA_query_feats, testB_query_feats), axis=0)
     IMAGE_NAME = copy.deepcopy(testA_query_imgnames)
-    IMAGE_NAME.append(testB_query_imgnames)
+    IMAGE_NAME.extend(testB_query_imgnames)
     clean_query_id_set = get_clean_query(QUERY_FEAT, IMAGE_NAME, query_dirty_threshold)
 
     # f = open('results/rerank_503.json', encoding='utf-8')
