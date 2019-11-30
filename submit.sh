85751ef8e4739a88ac92ad23988334ac80bed716
anxiang <anxiangsir@outlook.com>
2019-11-30 15:10:25 +0800

    reran

--- submit.sh
+++ submit.sh
@@ -4,8 +4,6 @@
 config_file=configs/${1}.yml
 # output_dir
 output_dir=/mnt/anxiang/models/reid_exp/${1}
-# train_root
-train_root=/home/xiangan/code_and_data/train_split/all_final
 # test_root
 test_root=/home/xiangan/data_reid/testB
 # model_pretrain_path
@@ -14,11 +12,6 @@ model_pretrain_path=${output_dir}/se_resnet50_model_120.pth
 echo "config files is ${config_file}"
 echo "save path is ${output_dir}"
 
-python tools/train.py \
-    --config_file=${config_file} \
-    MODEL.DEVICE_ID "('0,1')" \
-    DATASETS.ROOT_DIR "('${train_root}')" \
-    OUTPUT_DIR "('${output_dir}')"
 
 python extract_final.py \
     --config_file=${config_file} \
