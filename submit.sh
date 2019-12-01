f78efca441255180acca8093a72ac3976f8f78fe
anxiang <anxiangsir@outlook.com>
2019-12-01 22:18:36 +0800

    reran

--- submit.sh
+++ submit.sh
@@ -3,11 +3,11 @@
 # config_file
 config_file=configs/${1}.yml
 # output_dir
-output_dir=/data/xiangan/models/reid_exp/${1}
+output_dir=/data/xiangan/models/reid//${1}
 # train_root
 train_root=/data/xiangan/reid_data/trainset
 # model_path
-model_path=${output_dir}/se_resnet50_model_120.pth
+model_path=${output_dir}/se_resnet50_model_40.pth
 
 echo "config files is ${config_file}"
 echo "save path is ${output_dir}"
