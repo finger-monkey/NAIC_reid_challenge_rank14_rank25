d3312430b8052bec6e02cae4fcd586259364af75
anxiang <anxiangsir@outlook.com>
2019-12-03 17:26:02 +0800

    add search margin

--- four_fold_cross_validation.py
+++ four_fold_cross_validation.py
@@ -1,4 +1,6 @@
 import os
+import random
+from sklearn.model_selection import KFold
 
 # all dataset root
 ALL_ROOT = "/train/trainset/1/datasets/trainset/bounding_box_train"
@@ -14,27 +16,37 @@ def main():
         _label = img_name.split('_')[0]
         if _label in LABEL_COUNT:
             LABEL_COUNT[_label] += 1
-        else:
-            LABEL_COUNT[_label] = 1
 
-    def _split(lucky_num: int) -> tuple:
+    # shuffle image_list
+    random.shuffle(img_name_list)
+
+    # label list
+    label_list = list(LABEL_COUNT.keys())
+
+    def _split(train_index, test_index) -> tuple:
         """split origin image list to train, query, gallery sets
         """
-        assert isinstance(lucky_num, int)
+        _train_id_list = [label_list[_i] for _i in train_index]
+        _test_id_list = [label_list[_i] for _i in test_index]
+
         _train_list = []
         _query_list = []
         _gallery_list = []
         _query_id_set = set()
         for _img_name in img_name_list:
+
+            # get label
             label = _img_name.split('_')[0]
-            if int(label) % 4 == lucky_num:  # lucky id
+            if label in _test_id_list:  # lucky id
                 if label not in _query_id_set and LABEL_COUNT[label] != 1:
                     _query_list.append(_img_name)
                     _query_id_set.add(label)
                 else:
                     _gallery_list.append(_img_name)
-            else:
+            elif label in _train_id_list:
                 _train_list.append(_img_name)
+            else:
+                raise ValueError
         return _train_list, _query_list, _gallery_list
 
     def _copy(source_root, target_root, image_list):
@@ -43,11 +55,12 @@ def main():
             target_path = os.path.join(target_root, _img)
             open(target_path, 'wb').write(open(source_path, 'rb').read())
 
-    for split_num in [0, 1, 2, 3]:
-        train_list, query_list, gallery_list = _split(split_num)
-        train_path = os.path.join(SPLIT_ROOT, "split%d/bounding_box_train" % (split_num + 1))
-        query_path = os.path.join(SPLIT_ROOT, "split%d/query" % (split_num + 1))
-        gallery_path = os.path.join(SPLIT_ROOT, "split%d/bounding_box_test" % (split_num + 1))
+    kfold = KFold(n_splits=4, shuffle=True, random_state=10086)
+    for idx, (train_index, test_index) in enumerate(kfold.split(label_list)):
+        train_list, query_list, gallery_list = _split(train_index, test_index)
+        train_path = os.path.join(SPLIT_ROOT, "split%d/bounding_box_train" % (idx + 1))
+        query_path = os.path.join(SPLIT_ROOT, "split%d/query" % (idx + 1))
+        gallery_path = os.path.join(SPLIT_ROOT, "split%d/bounding_box_test" % (idx + 1))
 
         os.makedirs(train_path)
         os.makedirs(query_path)
