9d2337270bed3b95251845fb38fae203ac4a1dcc
anxiang <anxiangsir@outlook.com>
2019-12-06 19:15:18 +0800

    add mcw config files

--- data/transforms/build.py
+++ data/transforms/build.py
@@ -8,12 +8,15 @@ from .transforms import RandomPatch
 def build_transforms(cfg, is_train=True):
     normalize_transform = T.Normalize(mean=cfg.INPUT.PIXEL_MEAN, std=cfg.INPUT.PIXEL_STD)
     if is_train:
+
         transform = T.Compose([
             T.Resize(cfg.INPUT.SIZE_TRAIN),
             T.RandomHorizontalFlip(p=cfg.INPUT.PROB),
             T.Pad(cfg.INPUT.PADDING),
             T.RandomCrop(cfg.INPUT.SIZE_TRAIN),
-            RandomPatch(prob_happen=cfg.INPUT.RANDOM_PATCH_PROB, patch_max_area=0.1677),
+            RandomPatch(
+                prob_happen=cfg.INPUT.RANDOM_PATCH_PROB,
+                patch_max_area=cfg.INPUT.RANDOM_PATCH_AREA),
             T.ToTensor(),
             normalize_transform
         ])
