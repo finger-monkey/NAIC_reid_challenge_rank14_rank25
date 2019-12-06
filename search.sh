4dabce07d890f269d23f72ff19f9e2f75a6389be
anxiang <anxiangsir@outlook.com>
2019-12-06 17:26:04 +0800

    add mcw config files

--- search.sh
+++ search.sh
@@ -1,7 +1,7 @@
 # exec_file
 exec_file=$1
 
-for config_file in {"margin001","margin002","margin003","margin004","margin005"}; do
-    echo "${exec_file}"
-    bash "${exec_file}" ${config_file}
+for config_file in {"mcw001","mcw002","mcw003","mcw004","mcw005","mcw006","mcw007","mcw008","mcw009","mcw010","mcw011"}; do
+  echo "${exec_file}"
+  bash "${exec_file}" ${config_file}
 done
