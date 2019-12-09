b6b7229bbf5188ccd1d0da79ebb939f6cd61c356
anxiang <anxiangsir@outlook.com>
2019-12-09 21:37:32 +0800

    remove files

--- dgnet_val2.sh
+++ dgnet_val2.sh
@@ -3,9 +3,9 @@
 # config_file
 config_file=configs/${1}.yml
 # output_dir
-output_dir=/data/xiangan/models/reid/${1}_split1
+output_dir=./reid/${1}_split2
 # train_root
-train_root=/data/xiangan/reid_data/split1
+train_root=/root/train_split4_rematch/split2
 
 echo "config files is ${config_file}"
 echo "save path is ${output_dir}"
@@ -13,4 +13,5 @@ echo "save path is ${output_dir}"
 python tools/train.py --config_file=${config_file} \
     DATASETS.ROOT_DIR "('${train_root}')" \
     OUTPUT_DIR "('${output_dir}')" \
-    MODEL.DEVICE_ID "('0,1')"
+    MODEL.DEVICE_ID "('0,1,2,3')" \
+    MODEL.PRETRAIN_PATH 'se_resnet50-ce0d4300.pth'
