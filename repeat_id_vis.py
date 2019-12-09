5705fba59046c7411be919b1046e13e8c4a4eceb
anxiang <anxiangsir@outlook.com>
2019-12-09 17:23:23 +0800

    add experiment;

---
+++ repeat_id_vis.py
@@ -0,0 +1,11 @@
+import os
+
+
+def main():
+    image_1 = ['train/105180993.png', '2164']
+    image_2 = ['train/829283568.png', '3180']
+    image_3 = ['train/943445997.png', '3369']
+
+
+if __name__ == '__main__':
+    main()
