b9100f02b038ae3595a9affd3b8ceeb215f5a991
anxiang <anxiangsir@outlook.com>
2019-12-09 22:16:25 +0800

    modify train shell

--- dgnet_val1.sh
+++ dgnet_val1.sh
@@ -3,7 +3,7 @@
 # config_file
 config_file=configs/${1}.yml
 # output_dir
-output_dir=/data/xiangan/models/reid/${1}_split1
+output_dir=/train/trainset/1/models/reid_exp/${1}_split1
 # train_root
 train_root=/root/train_split4_rematch/split1
 
