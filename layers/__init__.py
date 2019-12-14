0a90bbed6f89553f0bcac101baa1fc66347859c0
anxiang <anxiangsir@outlook.com>
2019-12-15 00:44:50 +0800

    add arcface loss

--- layers/__init__.py
+++ layers/__init__.py
@@ -6,6 +6,7 @@
 
 import torch.nn.functional as F
 
+from .arcface_loss import ArcfaceLoss
 from .triplet_loss import TripletLoss, CrossEntropyLabelSmooth
 from .center_loss import CenterLoss
 from .focal_loss import FocalLoss
@@ -16,33 +17,57 @@ def make_loss(cfg, num_classes):  # modified by gu
     if cfg.MODEL.METRIC_LOSS_TYPE == 'triplet':
         triplet = TripletLoss(cfg.SOLVER.MARGIN)  # triplet loss
     else:
-        print('expected METRIC_LOSS_TYPE should be tripletbut got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
+        raise ValueError
+
+    if cfg.MODEL.IF_WITH_ARC_FACE_LOSS == 'yes':
+        arcface = ArcfaceLoss(
+            embedding_size=2048,
+            class_num=num_classes,
+            s=64,
+            m=0.5
+        )
 
     if cfg.MODEL.IF_LABELSMOOTH == 'on':
-        xent = CrossEntropyLabelSmooth(num_classes=num_classes)  # new add by luo
-        print("label smooth on, numclasses:", num_classes)
+        raise ValueError
+        # xent = CrossEntropyLabelSmooth(num_classes=num_classes)  # new add by luo
+        # print("label smooth on, numclasses:", num_classes)
 
     if sampler == 'softmax':
-        def loss_func(score, feat, target):
-            return F.cross_entropy(score, target)
+        raise ValueError
+        # def loss_func(score, feat, target):
+        #     return F.cross_entropy(score, target)
     elif cfg.DATALOADER.SAMPLER == 'triplet':
-        def loss_func(score, feat, target):
-            return triplet(feat, target)[0]
+        raise ValueError
+        # def loss_func(score, feat, target):
+        #     return triplet(feat, target)[0]
     elif cfg.DATALOADER.SAMPLER == 'softmax_triplet':
         def loss_func(score, feat, target):
             # remove final feature
+            final_feature = feat[-1]
             feat = feat[0:3]
             if cfg.MODEL.METRIC_LOSS_TYPE == 'triplet':
                 if cfg.MODEL.IF_LABELSMOOTH == 'on':
                     raise ValueError
                 else:
-                    return sum(F.cross_entropy(s, target) for s in score) + sum(
-                        triplet(f, target)[0] for f in feat)
+                    if cfg.MODEL.IF_WITH_ARC_FACE_LOSS == 'yes':
+
+                        # get arc-face feat
+                        arcface_feat = arcface(final_feature, target)
+                        return sum(F.cross_entropy(s, target) for s in score) + \
+                               sum(triplet(f, target)[0] for f in feat) + \
+                               F.cross_entropy(arcface_feat, target)
+
+                    elif cfg.MODEL.IF_WITH_ARC_FACE_LOSS == 'no':
+                        return sum(F.cross_entropy(s, target) for s in score) + \
+                               sum(triplet(f, target)[0] for f in feat)
+                    else:
+                        raise ValueError
             else:
-                print('expected METRIC_LOSS_TYPE should be triplet but got {}'.format(cfg.MODEL.METRIC_LOSS_TYPE))
+                raise ValueError
     else:
-        print('expected sampler should be softmax, triplet or softmax_triplet, '
-              'but got {}'.format(cfg.DATALOADER.SAMPLER))
+        raise ValueError
+        # print('expected sampler should be softmax, triplet or softmax_triplet, '
+        #       'but got {}'.format(cfg.DATALOADER.SAMPLER))
 
     return loss_func
 
