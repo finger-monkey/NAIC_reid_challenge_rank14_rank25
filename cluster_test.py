d86f863a7024e96619bde7843229ffd38a4fc500
anxiang <anxiangsir@outlook.com>
2019-12-17 17:55:42 +0800

    add arcface loss

--- cluster_test.py
+++ cluster_test.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import json
 
 import numpy as np
 from sklearn import preprocessing
@@ -15,7 +16,6 @@ def process_info(info):
 
 
 def visualization(rank_dict, test_root, output_root):
-
     for i in rank_dict.keys():
         each_folder = os.path.join(output_root, "%d_%s" % (len(rank_dict[i]), str(i)))
         os.makedirs(each_folder)
@@ -28,12 +28,11 @@ def visualization(rank_dict, test_root, output_root):
 def main():
     # threshold
     rank_dirty_threshold = 0.7
-    merge_threshold = 0.6
+    merge_threshold = 0.5
     #
     # testA rank_list
-    testA_ranklist = 'results/rerank_503_testA.json'
-    # testB rank_list
-    testB_ranklist = 'results/rerank_503_testB.json'
+    testA_ranklist = 'submission_jiankang_train_001.json'
+
     # testA
     testA_gallery_info = pickle.load(open(
         '/home/xiangan/dgreid/features/jiankang_train_001/gallery_feature.feat', 'rb')
@@ -49,7 +48,7 @@ def main():
         n_clusters=None,
         linkage='average',
         affinity="cosine",
-        distance_threshold=0.55
+        distance_threshold=merge_threshold
     )
 
     cls.fit(testA_query_feats)
@@ -62,51 +61,49 @@ def main():
             assert isinstance(LABEL_DICT[_label], list)
             LABEL_DICT[_label].append(testA_query_img_names[idx])
 
-    visualization(
-        LABEL_DICT,
-        "/data/xiangan/reid_final/test/query_a",
-        "/data/xiangan/vis_query"
-    )
+    # visualization(
+    #     LABEL_DICT,
+    #     "/data/xiangan/reid_final/test/query_a",
+    #     "/data/xiangan/vis_query"
+    # )
 
-    # # testB
-    # testB_gallery_info = pickle.load(open('/home/xiangan/dgreid/features_testB/503/gallery_b_feature.feat', 'rb'))
-    # testB_query_info = pickle.load(open('/home/xiangan/dgreid/features_testB/503/query_b_feature.feat', 'rb'))
+    f = open(testA_ranklist, encoding='utf-8')
+    content = f.read()
+    dic = json.loads(content)
+    cleaned_rank_dict_testA = {}
+    cleaned_count = 0
+    for i in LABEL_DICT.keys():
+        cur_query_name = LABEL_DICT[i][0]
+
+        query_cur_feat = testA_query_feats[testA_query_img_names.index(cur_query_name)]
+        cleaned_ranklist = []
+        origin_ranklist = dic[i][:10]
+
+        for gallery_name in origin_ranklist:
+            rank_cur_feat = testA_gallery_feats[testA_gallery_img_names.index(gallery_name)]
+            score = np.dot(query_cur_feat, rank_cur_feat)
+            if score > rank_dirty_threshold:
+                cleaned_ranklist.append(gallery_name)
+                cleaned_count += 1
+            else:
+                break
+        cleaned_rank_dict_testA[cur_query_name] = cleaned_ranklist
+    # for i in list(dic.keys()):
+    #     origin_ranklist = dic[i][:10]
+    #     query_cur_feat = testA_query_feats[testA_query_img_names.index(query_name)]
     #
-    # testB_gallery_feats, testB_gallery_imgnames = process_info(testB_gallery_info)
-    # testB_query_feats, testB_query_imgnames = process_info(testB_query_info)
-    #
-    # assert testA_gallery_feats.shape[0] == 5366
-    # assert testA_query_feats.shape[0] == 1348
-    # assert testB_gallery_feats.shape[0] == 13099
-    # assert testB_query_feats.shape[0] == 3147
-    #
-    # QUERY_FEAT = np.concatenate((testA_query_feats, testB_query_feats), axis=0)
-    # IMAGE_NAME = copy.deepcopy(testA_query_imgnames)
-    # IMAGE_NAME.extend(testB_query_imgnames)
-    # clean_query_id_set = get_clean_query(QUERY_FEAT, IMAGE_NAME, query_dirty_threshold)
-    #
-    # f = open(testA_ranklist, encoding='utf-8')
-    # content = f.read()
-    # dic = json.loads(content)
-    # cleaned_rank_dict_testA = {}
-    # for query_name in list(dic.keys()):
-    #     print(query_name)
-    #     if query_name in clean_query_id_set:
-    #         origin_ranklist = dic[query_name][:10]
-    #         query_cur_feat = testA_query_feats[testA_query_imgnames.index(query_name)]
-    #
-    #         cleaned_ranklist = []
-    #         cleaned_count = 0
-    #         for gallery_name in origin_ranklist:
-    #             rank_cur_feat = testA_gallery_feats[testA_gallery_imgnames.index(gallery_name)]
-    #             score = np.dot(query_cur_feat, rank_cur_feat)
-    #             if score > rank_dirty_threshold:
-    #                 cleaned_ranklist.append(gallery_name)
-    #                 cleaned_count += 1
-    #             else:
-    #                 break
-    #         cleaned_rank_dict_testA[query_name] = cleaned_ranklist
-    #         print(cleaned_count)
+    #     cleaned_ranklist = []
+    #     cleaned_count = 0
+    #     for gallery_name in origin_ranklist:
+    #         rank_cur_feat = testA_gallery_feats[testA_gallery_imgnames.index(gallery_name)]
+    #         score = np.dot(query_cur_feat, rank_cur_feat)
+    #         if score > rank_dirty_threshold:
+    #             cleaned_ranklist.append(gallery_name)
+    #             cleaned_count += 1
+    #         else:
+    #             break
+    #     cleaned_rank_dict_testA[query_name] = cleaned_ranklist
+    #     print(cleaned_count)
     #
     # f = open(testB_ranklist, encoding='utf-8')
     # content = f.read()
