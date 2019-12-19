ffa4e5bd548cbab51657d68ae6c8fdea9e5dba66
anxiang <anxiangsir@outlook.com>
2019-12-19 17:53:25 +0800

    add sync bn

--- modeling/backbones/senet.py
+++ modeling/backbones/senet.py
@@ -345,6 +345,10 @@ class SENet(nn.Module):
     
     def load_param(self, model_path):
         param_dict = torch.load(model_path)
+
+        if not isinstance(param_dict, dict):
+            param_dict = param_dict.state_dict()
+
         for i in param_dict:
             if 'last_linear' in i:
                 continue
