48f9074e9a890c1b372da48de65294518923cd46
anxiang <anxiangsir@outlook.com>
2019-12-21 13:35:13 +0800

    modify print

--- dgnet_all_train.sh
+++ dgnet_all_train.sh
@@ -13,5 +13,4 @@ echo "save path is ${output_dir}"
 python tools/train.py --config_file=${config_file} \
     DATASETS.ROOT_DIR "('${train_root}')" \
     OUTPUT_DIR "('${output_dir}')" \
-    MODEL.DEVICE_ID "('0,1,2,3')" \
-    MODEL.PRETRAIN_PATH 'se_resnet50-ce0d4300.pth'
+    MODEL.DEVICE_ID "('0')"
