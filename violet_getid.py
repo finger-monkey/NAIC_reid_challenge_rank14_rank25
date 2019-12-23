d88bdb8f9722c3814b5cdd5be7cdf50b05af94b9
anxiang <anxiangsir@outlook.com>
2019-12-24 01:03:31 +0800

    -_-

--- violet_getid.py
+++ violet_getid.py
@@ -3,7 +3,15 @@ import os
 import shutil
 from numpy import mean
 from tqdm import tqdm
+
 ROOT = '/data/xiangan/reid_final/all/bounding_box_train'
+
+#
+DELETE_GALLERY_ROOT = "/data/xiangan/reid_final/violet_origin/bounding_box_test"
+DELETE_QUERY_ROOT = "/data/xiangan/reid_final/violet_origin/query"
+
+#
+DELETE_TRAIN_ROOT = "/data/xiangan/reid_final/violet_del_train/bounding_box_train"
 
 
 def info(image_path):
@@ -21,6 +29,15 @@ def check(image_path, threshold=300):
     return True if sum(info(image_path)) > threshold else False
 
 
+def delete_img(delete_id_set, image_root):
+    image_name_list = os.listdir(image_root)
+    for image_name in tqdm(image_name_list):
+        image_path = os.path.join(image_root, image_name)
+        p_id = image_name.split('_')[0]
+        if p_id in delete_id_set:
+            os.unlink(image_path)
+
+
 def main():
     violet_set = set()
     image_name_list = os.listdir(ROOT)
@@ -35,6 +52,9 @@ def main():
             violet_set.add(p_id)
     for p_id in violet_set:
         print(p_id)
+    delete_img(violet_set, DELETE_GALLERY_ROOT)
+    delete_img(violet_set, DELETE_QUERY_ROOT)
+    delete_img(violet_set, DELETE_TRAIN_ROOT)
 
 
 if __name__ == '__main__':
