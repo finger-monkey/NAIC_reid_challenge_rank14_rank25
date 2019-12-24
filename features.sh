5d763323ebfc391b2d778ec2e6e67c3010c5126c
anxiang <anxiangsir@outlook.com>
2019-12-24 13:51:15 +0800

    -_-

--- features.sh
+++ features.sh
@@ -15,6 +15,6 @@ python get_features_violet.py \
     DATASETS.ROOT_DIR /data/xiangan/reid_final/violet_another \
     MODEL.PRETRAIN_CHOICE "('self')" \
     TEST.WEIGHT "('/data/xiangan/models/reid/violet_003_all/se_resnet50_model_80.pth')" \
-    OUTPUT_DIR "('./features/apex_003_test_violet')"
+    OUTPUT_DIR "('./features/violet_003_test_another')"
 
 
