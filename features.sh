d1e7c9baa9792b9f0c7e104cf96163a080690f0d
anxiang <anxiangsir@outlook.com>
2019-12-25 22:31:57 +0800

    -_-

--- features.sh
+++ features.sh
@@ -6,7 +6,7 @@ dataset="/data/xiangan/reid_final/all"
 model=/data/xiangan/models/reid/apex_003_all/se_resnet50_model_80.pth
 
 python get_features_origin.py \
-    --config_file= $config_file \
+    --config_file=$config_file \
     MODEL.DEVICE_ID "('0')" \
     DATASETS.ROOT_DIR  $dataset \
     MODEL.PRETRAIN_CHOICE "('self')" \
