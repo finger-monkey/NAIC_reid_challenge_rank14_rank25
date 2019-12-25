3a517c36c87173e474e96bb6a4b1946232a732d2
anxiang <anxiangsir@outlook.com>
2019-12-25 23:05:09 +0800

    -_-

--- features.sh
+++ features.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/bash
 
 
-config_file="configs/jiankang_train_001.yml"
+#config_file="configs/apex_003.yml"
 dataset="/data/xiangan/reid_final/all"
-model=/data/xiangan/models/reid/jiankang_train_001_all/se_resnet101_model_80.pth
+#model=/data/xiangan/models/reid/apex_003_all/se_resnet50_model_80.pth
 
-python get_features_origin.py \
-    --config_file=$config_file \
-    MODEL.DEVICE_ID "('0')" \
-    DATASETS.ROOT_DIR  $dataset \
-    MODEL.PRETRAIN_CHOICE "('self')" \
-    TEST.WEIGHT "('${model}')" \
-    OUTPUT_DIR "('./features/jiankang_train_001_test_origin')"
+#python get_features_origin.py \
+#    --config_file=$config_file \
+#    MODEL.DEVICE_ID "('0')" \
+#    DATASETS.ROOT_DIR  $dataset \
+#    MODEL.PRETRAIN_CHOICE "('self')" \
+#    TEST.WEIGHT "('${model}')" \
+#    OUTPUT_DIR "('./features/apex_003_test_origin')"
 
 
 python get_features_violet.py \
-    --config_file=$config_file \
+    --config_file="configs/jiankang_train_001.yml" \
     MODEL.DEVICE_ID "('0')" \
     DATASETS.ROOT_DIR $dataset \
     MODEL.PRETRAIN_CHOICE "('self')" \
-    TEST.WEIGHT "('${model}')" \
+    TEST.WEIGHT "('/data/xiangan/models/reid/jiankang_train_001/se_resnet101_model_80.pth')" \
     OUTPUT_DIR "('./features/jiankang_train_001_test_violet')"
 
 
