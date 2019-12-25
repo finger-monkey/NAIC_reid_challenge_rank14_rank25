e7516836b0880e2ef382ea1e68500a38f5bb8ebd
anxiang <anxiangsir@outlook.com>
2019-12-25 22:25:39 +0800

    -_-

--- features.sh
+++ features.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/bash
 
+
+config_file="configs/apex_003.yml"
+dataset="/data/xiangan/reid_final/all"
+model=/data/xiangan/models/reid/apex_003_all/se_resnet50_model_80.pth
+
 python get_features_origin.py \
-    --config_file=configs/apex_003.yml \
+    --config_file= $config_file\
     MODEL.DEVICE_ID "('0')" \
-    DATASETS.ROOT_DIR /data/xiangan/reid_final/all \
+    DATASETS.ROOT_DIR  $dataset\
     MODEL.PRETRAIN_CHOICE "('self')" \
-    TEST.WEIGHT "('/data/xiangan/models/reid/apex_003_all/se_resnet50_model_80.pth')" \
+    TEST.WEIGHT "('${model}')" \
     OUTPUT_DIR "('./features/apex_003_test_origin')"
 
 
 python get_features_violet.py \
-    --config_file=configs/violet_003.yml \
+    --config_file=configs/apex_003.yml \
     MODEL.DEVICE_ID "('0')" \
-    DATASETS.ROOT_DIR /data/xiangan/reid_final/violet_another \
+    DATASETS.ROOT_DIR $dataset \
     MODEL.PRETRAIN_CHOICE "('self')" \
-    TEST.WEIGHT "('/data/xiangan/models/reid/violet_003_all/se_resnet50_model_80.pth')" \
-    OUTPUT_DIR "('./features/violet_003_test_another')"
+    TEST.WEIGHT "('${model}')" \
+    OUTPUT_DIR "('./features/apex_003_test_violet')"
 
 
