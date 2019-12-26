a1a6d22521d64013afce7a6d2ceafffbf3c5d5cc
anxiang <anxiangsir@outlook.com>
2019-12-27 00:31:25 +0800

    -_-

--- all_train.sh
+++ all_train.sh
@@ -3,9 +3,9 @@
 # config_file
 config_file=configs/${1}.yml
 # output_dir
-output_dir=/raid/jiankangdeng/reid/models/${1}_all_5000
+output_dir=/raid/jiankangdeng/reid/models/${1}_all_12_27
 # train_root
-train_root=/raid/jiankangdeng/reid/train_split4_rematch/all_5000
+train_root=/raid/jiankangdeng/reid/train_split4_rematch/all_12_27
 
 echo "config files is ${config_file}"
 echo "save path is ${output_dir}"
@@ -13,4 +13,4 @@ echo "save path is ${output_dir}"
 python tools/train.py --config_file=${config_file} \
     DATASETS.ROOT_DIR "('${train_root}')" \
     OUTPUT_DIR "('${output_dir}')" \
-    MODEL.DEVICE_ID "('0,1,2,3')"
+    MODEL.DEVICE_ID "('0')"
