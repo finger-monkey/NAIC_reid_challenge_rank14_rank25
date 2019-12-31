b71a1be72ba6244783ba5c2e8e038b31dc724774
anxiang <anxiangsir@outlook.com>
2019-12-31 23:08:14 +0800

    Merge branch 'MGN' into dgnet_root
    # Conflicts:
    #	dgnet_val2.sh
    #	dgnet_val3.sh

--- all_only_testB_train.sh
+++ all_only_testB_train.sh
@@ -3,9 +3,9 @@
 # config_file
 config_file=configs/${1}.yml
 # output_dir
-output_dir=/raid/jiankangdeng/reid/models/${1}_all_12_27
+output_dir=/raid/jiankangdeng/reid/models/${1}_all_only_testB
 # train_root
-train_root=/raid/jiankangdeng/reid/train_split4_rematch/all_12_27
+train_root=/raid/jiankangdeng/reid/train_split4_rematch/all_only_testB
 
 echo "config files is ${config_file}"
 echo "save path is ${output_dir}"
@@ -13,4 +13,4 @@ echo "save path is ${output_dir}"
 python tools/train.py --config_file=${config_file} \
     DATASETS.ROOT_DIR "('${train_root}')" \
     OUTPUT_DIR "('${output_dir}')" \
-    MODEL.DEVICE_ID "('0')"
+    MODEL.DEVICE_ID "('3')"
