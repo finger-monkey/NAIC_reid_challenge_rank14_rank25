25f49e132bd642bed4a212b7a021bf37c518fd23
anxiang <anxiangsir@outlook.com>
2020-01-02 23:23:04 +0800

    Merge branch 'MGN' into dgnet_root
    # Conflicts:
    #	dgnet_val2.sh
    #	dgnet_val3.sh

--- reranking_submit.py
+++ reranking_submit.py
@@ -6,8 +6,8 @@ import torch
 from sklearn import preprocessing
 from rerank.rerank_kreciprocal import re_ranking
 from sklearn.cluster import AgglomerativeClustering
-
 NAME = "dgreid_b_003"
+
 
 LABEL_DICT = {}
 
@@ -26,7 +26,7 @@ def get_result(query_imgnames, query_feats, gallery_feats, gallery_imgnames):
     clean_set = set()
     #
     for q_idx in range(num_q):
-        order = indices[q_idx][:200]
+        order = indices[q_idx][:100]
         for gallery_index in order:
             clean_set.add(gallery_imgnames[gallery_index])
         else:
@@ -68,52 +68,51 @@ def main():
     gallery_feats, gallery_imgnames = process_info(gallery_info)
     query_feats, query_imgnames = process_info(query_info)
 
-    # #
-    # chunk_size = 1000
-    # #
-    # iter_num = len(query_imgnames) // chunk_size + 1
-    # #
-    # submission_key = {}
-    # #
-    # for i in range(iter_num):
-    #     if i == iter_num - 1:
-    #         _query_imgnames = query_imgnames[i * chunk_size:]
-    #         _query_feats = query_feats[i * chunk_size:]
-    #     else:
-    #         _query_imgnames = query_imgnames[i * chunk_size: (i + 1) * chunk_size]
-    #         _query_feats = query_feats[i * chunk_size: (i + 1) * chunk_size]
     #
-    #     # get
-    #     _submission_key = get_result(_query_imgnames, _query_feats, gallery_feats, gallery_imgnames)
-    #     # update
-    #     submission_key.update(_submission_key)
-
-    cls = AgglomerativeClustering(
-        n_clusters=10,
-        linkage='average',
-        affinity="cosine",
-    )
-
-    cls.fit(query_feats)
-
-    # generate label dict
-    for idx, _label in enumerate(cls.labels_):
-        if _label not in LABEL_DICT:
-            LABEL_DICT[_label] = [query_imgnames[idx]]
+    chunk_size = 500
+    #
+    iter_num = len(query_imgnames) // chunk_size + 1
+    #
+    submission_key = {}
+    #
+    for i in range(iter_num):
+        if i == iter_num - 1:
+            _query_imgnames = query_imgnames[i * chunk_size:]
+            _query_feats = query_feats[i * chunk_size:]
         else:
-            assert isinstance(LABEL_DICT[_label], list)
-            LABEL_DICT[_label].append(query_imgnames[idx])
-    #
-    print(len(set(cls.labels_)))
-    for id, l in LABEL_DICT.items():
-        print(len(l))
+            _query_imgnames = query_imgnames[i * chunk_size: (i + 1) * chunk_size]
+            _query_feats = query_feats[i * chunk_size: (i + 1) * chunk_size]
 
-    # # final
-    # submission_json = json.dumps(submission_key)
-    # print(type(submission_json))
+        # get
+        _submission_key = get_result(_query_imgnames, _query_feats, gallery_feats, gallery_imgnames)
+        # update
+        submission_key.update(_submission_key)
+
+    # cls = AgglomerativeClustering(
+    #     n_clusters=None,
+    #     linkage='average',
+    #     affinity="cosine",
+    #     distance_threshold=0.7
+    # )
     #
-    # with open('rerank_%s.json' % NAME, 'w', encoding='utf-8') as f:
-    #     f.write(submission_json)
+    # cls.fit(query_feats)
+    #
+    # # generate label dict
+    # for idx, _label in enumerate(cls.labels_):
+    #     if _label not in LABEL_DICT:
+    #         LABEL_DICT[_label] = [testA_query_img_names[idx]]
+    #     else:
+    #         assert isinstance(LABEL_DICT[_label], list)
+    #         LABEL_DICT[_label].append(testA_query_img_names[idx])
+    # #
+    # print(len(set(cls.labels_)))
+
+    # final
+    submission_json = json.dumps(submission_key)
+    print(type(submission_json))
+
+    with open('rerank_%s.json' % NAME, 'w', encoding='utf-8') as f:
+        f.write(submission_json)
 
 
 if __name__ == '__main__':
