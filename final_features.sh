d620264e5ee8b9e8cc1ce9ffd8544b43ff69a119
anxiang <anxiangsir@outlook.com>
2020-01-06 01:36:45 +0800

    Merge branch 'MGN' into dgnet_root
    # Conflicts:
    #	dgnet_val2.sh
    #	dgnet_val3.sh

---
+++ final_features.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# config_file
+config_file=configs/model_final.yml
+# train_root
+train_root=/tmp/data/all
+# model_path
+model_path=/tmp/data/model/model_final/se_resnet50_model_80.pth
+
+
+python get_features.py \
+    --config_file=${config_file} \
+    MODEL.DEVICE_ID "('0')" \
+    DATASETS.ROOT_DIR ${train_root} \
+    MODEL.PRETRAIN_CHOICE "('self')" \
+    TEST.WEIGHT "('${model_path}')" \
+    OUTPUT_DIR "('/tmp/data/features/final')"
+
+
