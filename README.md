a71a291dbe85dc6d9a7d11c371e736a285f724d2
anxiang <anxiangsir@outlook.com>
2020-01-05 23:34:46 +0800

    Merge branch 'MGN' into dgnet_root
    # Conflicts:
    #	dgnet_val2.sh
    #	dgnet_val3.sh

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Experiment-Four-Fold
 
-1. 线下单卡batchsize 32
-2. 线下四卡batchsize 32 同步BN
-3. 清洗更多的测试集加入训练实验
-4. 提取训练集测试集特征给张勇，他负责更多的聚类
+1. 运行 `python convert2trainset.py` 生成训练集；
---
+++ convert2trainset.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+
+# config
+ORIGIN_TRAIN_ROOT = "/tmp/data/train/image"
+LABEL_FILE = "/tmp/data/label/train_list.txt"
+
+TRAIN_ROOT = "/tmp/data/all/bounding_box_train"
+
+_dirty_label = {
+    '105180993.png': '5721',
+    '829283568.png': '3180',
+    '943445997.png': '3369'
+}
+
+
+def main():
+    if not os.path.exists(TRAIN_ROOT):
+        os.makedirs(TRAIN_ROOT)
+
+    with open(LABEL_FILE, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            image_name = line.split(' ')[0].split('/')[1]
+            if image_name in _dirty_label.keys():
+                label = _dirty_label[image_name]
+            else:
+                label = line.split(' ')[1]
+
+            target_name = "%s_c1_%s" % (label, image_name)
+
+            source_path = os.path.join(ORIGIN_TRAIN_ROOT, image_name)
+            target_path = os.path.join(TRAIN_ROOT, target_name)
+            shutil.copy(source_path, target_path)
+
+
+if __name__ == '__main__':
+    main()
